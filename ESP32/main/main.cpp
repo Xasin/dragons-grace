b29cb6140e217acb336f5d6742c2da2e6c38dbc1
Xaseiresh <davidbailey.2889@gmail.com>
2021-12-03 09:23:42 +0100

    Add XNM OTA configuration

--- ESP32/main/main.cpp
+++ ESP32/main/main.cpp
@@ -12,44 +12,83 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "nvs_flash.h"
 
 #include <cmath>
 
-#include <xasin/neocontroller.h>
+#include <xnm/neocontroller.h>
 
-Xasin::NeoController::NeoController test(GPIO_NUM_8, RMT_CHANNEL_0, 16);
+#include <xnm/net_helpers.h>
+#include <xnm/i2c/AW9201.h>
+#include <xnm/i2c/MasterAction.h>
+
+#include <esp_log.h>
+
+XNM::Neo::NeoController test(GPIO_NUM_6, RMT_CHANNEL_0, 20);
+
+XNM::I2C::AW9201 booper;
+
+esp_err_t event_handler(void *context, system_event_t *event)
+{
+    XNM::NetHelpers::event_handler(event);
+    return ESP_OK;
+}
+
+void flash_task(void *args) {
+    test.init();
+
+    while(true) {
+        vTaskDelay(25/portTICK_PERIOD_MS);
+
+        for (int i = 0; i < test.colors.length(); i++)
+            test.colors[i] = XNM::Neo::Color::HSV(xTaskGetTickCount() / 30.0F, 255, 60);
+
+        test.update();
+    }
+}
 
 extern "C"
 void app_main(void)
 {
-    printf("Hello world!\n");
+    XNM::I2C::MasterAction::init(GPIO_NUM_8, GPIO_NUM_2, 0, 10000);
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+    // Initialize NVS — it is used to store PHY calibration data
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    esp_event_loop_create_default();
+    esp_event_loop_init(event_handler, nullptr);
 
     printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
 
-    test.colors.fill(Material::RED);
-    test.update();
+    xTaskCreate(flash_task, "LEDs", 4096, nullptr, 20, nullptr);
 
-    for (int i = 10000; i >= 0; i--) {
+    XNM::NetHelpers::WIFI::set_nvs("TP-LINK_84CDC2", "f36eebda48");
+    Xasin::MQTT::Handler::set_nvs_uri("mqtts://xaseiresh.hopto.org");
+
+    XNM::NetHelpers::init_global_r3_ca();
+    XNM::NetHelpers::init();
+
+    booper.init();
+
+    for(int i=0; i<127; i++) {
         vTaskDelay(10);
 
-        auto c = Xasin::NeoController::Color::HSV(xTaskGetTickCount() / 100.0F);
+        if(XNM::I2C::MasterAction::poke(i) == ESP_OK)
+            ESP_LOGI("I2C", "Got address %x", i);
+    }
 
-        test.colors.fill(c);
-        test.update();
+    while(true) {
+       vTaskDelay(100);
+
+        booper.update();
+        if(booper.is_touched())
+            ESP_LOGI("BOOPR", "I am %stouched! Value is %d", booper.is_touched() ? "" : "not ", booper.cap_value());
     }
 
     printf("Restarting now.\n");
